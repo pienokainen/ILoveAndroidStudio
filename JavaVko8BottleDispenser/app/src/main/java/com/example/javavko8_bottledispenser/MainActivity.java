332d09abafe2a8447f415ff0ff0c7681c358365b
pienokainen <77006105+pienokainen@users.noreply.github.com>
2021-03-10 09:40:08 +0200

    New project created :]

---
+++ JavaVko8BottleDispenser/app/src/main/java/com/example/javavko8_bottledispenser/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.javavko8_bottledispenser;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
