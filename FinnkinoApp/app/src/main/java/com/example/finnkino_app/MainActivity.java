0fd18d6743ac23ad56fb1be66e6ed9bac462a3f8
pienokainen <77006105+pienokainen@users.noreply.github.com>
2021-03-18 17:22:54 +0200

    assignment 9.2 done!

--- FinnkinoApp/app/src/main/java/com/example/finnkino_app/MainActivity.java
+++ FinnkinoApp/app/src/main/java/com/example/finnkino_app/MainActivity.java
@@ -4,20 +4,29 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.os.StrictMode;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.ArrayList;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 
 public class MainActivity extends AppCompatActivity {
+    Spinner spinner;
+    private ArrayList<Theatre> theaters;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +34,15 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
         StrictMode.setThreadPolicy(policy);
+        spinner = (Spinner) findViewById(R.id.theater);
+
+        theaters = new ArrayList<Theatre>();
+        readXML();
+        ArrayAdapter<Theatre> adapter =
+                new ArrayAdapter<Theatre>(getApplicationContext(),  android.R.layout.simple_spinner_dropdown_item, theaters);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        spinner.setAdapter(adapter);
     }
 
     public void readXML() {
@@ -35,6 +53,23 @@ public class MainActivity extends AppCompatActivity {
             URL url = new URL("https://www.finnkino.fi/xml/TheatreAreas/");
             InputStream stream = url.openStream();
             Document doc = docBuilder.parse(stream);
+            doc.getDocumentElement().normalize();
+
+            NodeList nList = doc.getDocumentElement().getElementsByTagName("TheatreArea");
+            Theatre theatre;
+            for (int i = 0; i < nList.getLength(); i++) {
+                Node node = nList.item(i);
+                System.out.println("Teatterin koko tiedot: " + node.getNodeName());
+
+                if (node.getNodeType() == Node.ELEMENT_NODE) {
+                    Element element = (Element) node;
+                    String theatreName = element.getElementsByTagName("Name").item(0).getTextContent();
+                    int theatreID = Integer.parseInt(element.getElementsByTagName("ID").item(0).getTextContent());
+                    theatre = new Theatre(theatreName, theatreID);
+                    theaters.add(theatre);
+                }
+            }
+
         } catch (SAXException e) {
             e.printStackTrace();
         } catch (IOException e) {
---
+++ FinnkinoApp/app/src/main/java/com/example/finnkino_app/Theatre.java
@@ -0,0 +1,22 @@
+package com.example.finnkino_app;
+
+public class Theatre {
+    private String name;
+    private int id;
+
+    public Theatre(String theatreName, int theatreID){
+        name = theatreName;
+        id = theatreID;
+    }
+    public String getName(){
+        return name;
+    }
+    public double getID() {
+        return id;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+}
