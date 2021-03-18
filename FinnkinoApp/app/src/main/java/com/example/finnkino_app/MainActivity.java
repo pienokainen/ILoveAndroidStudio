96c80a73122211591e7b8ff772174d4ce12630c0
pienokainen <77006105+pienokainen@users.noreply.github.com>
2021-03-18 12:01:59 +0200

    Basic layout done and XML reading started

--- FinnkinoApp/app/src/main/java/com/example/finnkino_app/MainActivity.java
+++ FinnkinoApp/app/src/main/java/com/example/finnkino_app/MainActivity.java
@@ -3,6 +3,19 @@ package com.example.finnkino_app;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.StrictMode;
+
+import org.w3c.dom.Document;
+import org.xml.sax.SAXException;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +23,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
     }
+
+    public void readXML() {
+        try {
+            DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+            DocumentBuilder docBuilder = dbf.newDocumentBuilder();
+
+            URL url = new URL("https://www.finnkino.fi/xml/TheatreAreas/");
+            InputStream stream = url.openStream();
+            Document doc = docBuilder.parse(stream);
+        } catch (SAXException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ParserConfigurationException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
