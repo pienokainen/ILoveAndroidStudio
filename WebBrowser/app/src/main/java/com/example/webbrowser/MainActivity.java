a3a6e8af98705892d895e0a0a4d5c118922bc7ed
pienokainen <77006105+pienokainen@users.noreply.github.com>
2021-03-24 09:01:58 +0200

    wrote first few lines

--- WebBrowser/app/src/main/java/com/example/webbrowser/MainActivity.java
+++ WebBrowser/app/src/main/java/com/example/webbrowser/MainActivity.java
@@ -3,12 +3,19 @@ package com.example.webbrowser;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class MainActivity extends AppCompatActivity {
+    WebView web;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        web = findViewById(R.id.webView);
+        web.setWebViewClient(new WebViewClient());
+        web.getSettings().setJavaScriptEnabled(true);
     }
 }
